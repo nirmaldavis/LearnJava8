de4a58e87b1908c36ee5de30ce8d9df7f6026ff8
nirmaldavis <nirmal.davis@gmail.com>
2017-10-18 11:45:33 +0530

    Editing to test

--- HelloJava8/src/com/nirmal/hello/HelloJava.java
+++ HelloJava8/src/com/nirmal/hello/HelloJava.java
@@ -3,7 +3,7 @@ package com.nirmal.hello;
 public class HelloJava {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to world of Java 8..!!");
+		System.out.println("Welcome to world of Java 8..!! ");
 	}
 
 }
